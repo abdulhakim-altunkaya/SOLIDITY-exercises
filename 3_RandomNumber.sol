823ac3938b1ecd10573155a68ec04fb5b4c7c832
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-21 16:00:09 +0300

    4th exercise

--- 3_RandomNumber.sol
+++ 3_RandomNumber.sol
@@ -1,10 +1,10 @@
 pragma solidity >=0.8.7;
 
-contract Require {
-    uint private firstNumber = 0;
+contract RandomNumber {
+    uint public firstNumber = 0;
     
-    function createRandomNumber() {
-        uint randomNumber = uint(keccak256(abi.encodePacked(now, msg.sender, firstNumber))) % 100;
+    function createRandomNumber() public {
+        uint randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, firstNumber))) % 100;
         firstNumber = randomNumber;
     }
 }
---
+++ 4_Struct.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.7;
+
+contract PersonDetails {
+   struct Person { 
+      string name;
+      uint age;
+   }
+
+   
+}
