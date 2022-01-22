6484073fd35281804ee62e6e4a138fcc172ab4f7
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-22 22:39:10 +0300

    for loop code added

--- 6_ForLoop.sol
+++ 6_ForLoop.sol
@@ -4,7 +4,12 @@ contract FunctionOne {
     function getEvens() pure external returns(uint[] memory) {
         uint[] memory evens = new uint[](20);
         uint myIndex = 0;
-
+        for(uint i=1; i<=20; i++) {
+            if(i % 2 == 0) {
+                evens[myIndex] = i;
+                myIndex++;
+            }
+        }
         return evens;
     }
 }
