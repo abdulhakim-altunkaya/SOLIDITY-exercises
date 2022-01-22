0410b72a9ccecf2e3a984846aa3df7cd69aedd19
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-22 22:35:21 +0300

    new file added and for loop file created

---
+++ 6_ForLoop.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.7;
+
+contract FunctionOne {
+    function getEvens() pure external returns(uint[] memory) {
+        uint[] memory evens = new uint[](20);
+        uint myIndex = 0;
+
+        return evens;
+    }
+}
