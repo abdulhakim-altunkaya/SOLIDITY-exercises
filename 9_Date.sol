5b005028b150620cc37208bec39fcc5aa991791b
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-25 18:58:28 +0300

    function added to the Date contract

--- 9_Date.sol
+++ 9_Date.sol
@@ -1,10 +1,19 @@
 pragma solidity >=0.8.7;
 
 contract Date {
-    uint public lastMoment;
-    uint public minuteOne;
 
-    function getMinute() public {
-        minuteOne = 1 minutes;
+    uint public lastMoment;
+    uint public minutesOne;
+
+    function setTime() public {
+        lastMoment = block.timestamp;
     }
+    function setTime2() public {
+        minutesOne = 1 minutes;
+    }
+    
+    function isTimeFinished() public view returns(bool) {
+        return(block.timestamp > (lastMoment + minutesOne) );
+    }
+    
 }
