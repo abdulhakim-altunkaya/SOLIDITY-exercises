904ee871f3001794768665f1bf70a766dd9cab67
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-27 22:25:23 +0300

    while loop created

--- 11_whileLoop.sol
+++ 11_whileLoop.sol
@@ -4,5 +4,15 @@ contract Types {
     // creating a simple dynamic array and making it public so that you can see it in remix
     uint[] public data; 
     
+    uint j = 0;
+
+    function loop() public returns(uint[] memory){
+        while(j < 100) {
+            j = j + 7;
+            data.push(j);
+        }
+        return data;
+
+    }
 
 }
