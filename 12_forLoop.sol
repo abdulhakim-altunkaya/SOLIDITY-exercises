540809a5d4401f9699290d07451de05975c3aeb7
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-29 16:02:46 +0300

    file structure created

--- 12_forLoop.sol
+++ 12_forLoop.sol
@@ -1,5 +1,14 @@
 pragma solidity ^0.5.0; 
   
 contract forLoop { 
-
+    
+    uint[] public myArray;
+    uint baseNumber = 9;
+    function forLoop() public returns(uint[] memory) {
+        for (uint256 index = 0; index < 10; index++) {
+            baseNumber = baseNumber + 7;
+            myArray.push(baseNumber);
+        }
+        return myArray;
+    }
 }
