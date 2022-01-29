37a831c44d98e83776af4ec0f33db78801629a5d
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-29 21:15:18 +0300

    for loop function is finished

--- 12_forLoop.sol
+++ 12_forLoop.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.5.0;
 contract forLoop { 
     
     uint[] public myArray;
-    uint baseNumber = 9;
-    function forLoop() public returns(uint[] memory) {
+    uint public baseNumber = 9;
+    function createArray() public returns(uint[] memory) {
         for (uint256 index = 0; index < 10; index++) {
             baseNumber = baseNumber + 7;
             myArray.push(baseNumber);
