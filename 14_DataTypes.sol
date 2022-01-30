5c0fa85a60f7f95ac16f9e27e7b0bb28bb965382
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-30 21:17:04 +0300

    new file to analyze data types

---
+++ 14_DataTypes.sol
@@ -0,0 +1,14 @@
+pragma solidity >0.5.0;
+
+
+contract DataTypes {
+    
+    uint myNumber = 9;
+    int yourNumber = -68;
+    uint8 herNumber = 17;
+    bool isSolidityCool = true;
+    address owner = msg.sender;
+    bytes32 byteMes = "hello byte";
+    string stringMes = "hello string";
+
+}
