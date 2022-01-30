d700cc7a666431419646ae87da2ee0537f1e1a82
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-30 14:05:18 +0300

    basic math file finished and tested

--- 13_BasicMath.sol
+++ 13_BasicMath.sol
@@ -11,12 +11,13 @@ contract BasicMath {
     uint256 public variable_mul = 50;
     uint256 public variable_mod = 50;
 
-    function basicMath() public {
-        variable_add.add(9);
-        variable_sub.sub(9);
-        variable_mul.mul(9);
-        variable_div.div(9);
-        variable_mod.mod(9);
+    function basicMath() public { // I can add *return* here but it works without it
+        variable_add = variable_add.add(9);
+        variable_div = variable_div.div(9);
+        variable_mul = variable_mul.mul(9);
+        variable_sub = variable_sub.sub(9);
+        variable_mod = variable_mod % 9;
+        // I can add *return* here but it doesnt change anything
     }
 
 }
