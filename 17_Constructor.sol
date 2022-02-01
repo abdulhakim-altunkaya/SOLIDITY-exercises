a5e139c27114e10ea118a2042d983b27c28461b7
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-01 20:48:46 +0300

    solidirt constructor code improved

--- 17_Constructor.sol
+++ 17_Constructor.sol
@@ -1,10 +1,15 @@
-pragama solidity >= 0.8.7;
+pragma solidity >= 0.8.7;
 
 contract Constructor {
     string public myWord;
     string public myWord2;
 
+    constructor() public {
+        myWord = "Constructor works";
+        myWord = "Solidity is nice";
+    }
+
     function getWord() public view returns(string memory) {
-        return myWord;
+        return myWord2;
     }
 }
