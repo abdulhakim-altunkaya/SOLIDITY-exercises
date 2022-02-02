3b99a42a09c212df26c25745b98201e259d2e9b4
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-02 12:31:24 +0300

    functions developed

--- 18_Constructor2.sol
+++ 18_Constructor2.sol
@@ -3,5 +3,11 @@ pragma solidity >=0.8.7;
 contract RandomNumber {
     uint public myNumber = 8;
     uint public newNumber = 9;
-
+    constructor() {
+        myNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, myNumber))) % 100;
+    }
+    function addNumber(uint _value) public returns(uint) {
+        newNumber = newNumber + myNumber + _value;
+        return newNumber;
+    }
 }
