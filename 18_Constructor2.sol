07ed5b0216b430666f7bd92163aa3454faecf1c8
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-02 15:32:57 +0300

    constructor exercises improved

--- 18_Constructor2.sol
+++ 18_Constructor2.sol
@@ -3,11 +3,13 @@ pragma solidity >=0.8.7;
 contract RandomNumber {
     uint public myNumber = 8;
     uint public newNumber = 9;
+    uint public randomNumber;
     constructor() {
-        myNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, myNumber))) % 100;
+        randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, myNumber))) % 100;
+        myNumber = myNumber + randomNumber;
     }
     function addNumber(uint _value) public returns(uint) {
         newNumber = newNumber + myNumber + _value;
         return newNumber;
-    }
+    }  
 }
