3f50e981bd4d0c9183b87782bd7499f5e7e4f43e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-05 17:58:51 +0300

    bugs fixed and file tested on remix

--- 21_Array4.sol
+++ 21_Array4.sol
@@ -3,9 +3,11 @@ pragma solidity >=0.8.7;
 // This contract will get the highest number of an array
 contract GetHighestNumber {
     uint[3] public myArray = [42, 20, 36];
-    uint save = 0;
+    //Dont put uint save inside the function because, I want to assign
+    //each  true cycle value to the save.
+    uint public save = 0;
 
-    function getHighest() public view returns(uint[] memory) {
+    function getHighest() public returns(uint) {
         for (uint256 index = 0; index < myArray.length; index++) {
             if (myArray[index] >= save) {
                 save = myArray[index];
@@ -15,5 +17,7 @@ contract GetHighestNumber {
         }
         return save;
     }
-
+    // return statement must be after for statement because 
+    // because if i put it inside for statement, I will have 3 loop results
+    // But I want just one, not three
 }
