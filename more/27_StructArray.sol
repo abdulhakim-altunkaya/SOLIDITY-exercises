64d71e7d78b2eec80bf0244f4577d30f1627c65f
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-08 15:21:00 +0300

    for loop added to search and find a record

--- more/27_StructArray.sol
+++ more/27_StructArray.sol
@@ -8,5 +8,27 @@ contract Crud {
     UserStruct[] public myArray;
     uint public indexId;
 
+    function createUser(string memory newName) public {
+        myArray.push(UserStruct(indexId, newName));
+        indexId++;
+    }
+    function getAllArray() public view returns(UserStruct[] memory) {
+        return myArray;
+    }
 
+    function getElement(uint index) public view returns(UserStruct memory) {
+        for(uint i=0; i<myArray.length; i++) {
+            if(i == index) {
+                return myArray[index];
+            }
+        }
+    }
+
+    function updateName(uint number2, string memory freshName) public {
+        for(uint i=0; i<myArray.length; i++) {
+            if(i == number2) {
+                myArray[i].name = freshName;
+            }
+        }
+    }
 }
