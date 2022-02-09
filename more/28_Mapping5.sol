a443033e8f6b44e311ad9dffc8ec4d288a7ece4b
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-09 21:58:50 +0300

    function created to add into mapping

--- more/28_Mapping5.sol
+++ more/28_Mapping5.sol
@@ -3,5 +3,9 @@ pragma solidity >=0.8.7;
 contract LedgerBalance {
     mapping(address => uint) public ledgerBalances;
 
+    function (uint newBalance) public {
+        ledgerBalance[msg.sender] = newBalance;
+    }
+
 
 }
