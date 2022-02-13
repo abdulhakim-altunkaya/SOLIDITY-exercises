bbebf97032570fcd85ff82031e32bc265bbc7cda
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-14 00:11:14 +0300

    caller calle function created with simple function

--- more/32_Modifier3.sol
+++ more/32_Modifier3.sol
@@ -1,3 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+contract A {
+  address myAddress;
+  function setAddressB(address _addressB) external {
+    myAddress = _addressB
+  }
+  function callHelloWorld() external view returns(string memory) {
+    B b = B(myAddress);
+    return b.helloWorld();
+  }
+}
+
+contract B {
+  
+  function helloWorld() external pure returns(string memory) {
+    return "Hello World";
+  }
+
+}
