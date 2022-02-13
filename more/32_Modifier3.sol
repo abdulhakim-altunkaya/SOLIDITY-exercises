c717e3a35d45abfb0b837908f8e033b5d787aeef
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-13 23:09:15 +0300

    callee-caller structure recreated

--- more/32_Modifier3.sol
+++ more/32_Modifier3.sol
@@ -1,13 +1,34 @@
-pragma solidity >=0.8.7;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
-contract myContract { 
+contract Callee {
+    uint public x;
+    uint public value;
 
-   function f1(bool arg1, uint arg2) returns(uint);
+    function setX(uint _x) public returns (uint) {
+        x = _x;
+        return x;
+    }
+
+    function setXandSendEther(uint _x) public payable returns (uint, uint) {
+        x = _x;
+        value = msg.value;
+
+        return (x, value);
+    }
 }
 
-contract YourContract {
-  function doYourThing(address addressOfA) public returns(uint) {
-    A my_a = A(addressOfA);
-    return my_a.f1(true, 3);
-  }
+contract Caller {
+    function setX(Callee _callee, uint _x) public {
+        uint x = _callee.setX(_x);
+    }
+
+    function setXFromAddress(address _addr, uint _x) public {
+        Callee callee = Callee(_addr);
+        callee.setX(_x);
+    }
+
+    function setXandSendEther(Callee _callee, uint _x) public payable {
+        (uint x, uint value) = _callee.setXandSendEther{value: msg.value}(_x);
+    }
 }
