3bc827a05564346e70537edb7539085396f78c34
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-15 22:42:47 +0300

    bugs fixed

--- more/34_Multicon2-1.sol
+++ more/34_Multicon2-1.sol
@@ -6,17 +6,17 @@ contract A {
     address myAddress;
 
     function setAddress(address _newContract) external {
-        myAdress = _newContract;
+        myAddress = _newContract;
     }
 
-    function getText(string _anyText2) external view returns(string memory) {
+    function getText(string memory _anyText2) external view returns(string memory) {
         B b = B(myAddress);
         return b.getString(_anyText2);
     }
 }
 
 contract B {
-    function getString(string _anyText) external pure returns(string memory) {
+    function getString(string memory _anyText) external pure returns(string memory) {
         return _anyText;
     }
 }
