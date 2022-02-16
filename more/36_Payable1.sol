ee5e080e4a0bfa85eb73d4e157664757e072dab4
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-17 01:19:40 +0300

    functions created

--- more/36_Payable1.sol
+++ more/36_Payable1.sol
@@ -3,5 +3,11 @@
 pragma solidity >=0.8.7;
 
 contract SendEther {
-    
+    function sendEth() external payable {
+
+    }
+
+    function balanceOf() external view returns(uint) {
+        return address(this).balance;
+    }
 }
