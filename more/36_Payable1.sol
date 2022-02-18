0187f15477a7a2f71120e2855a1388485092d0e9
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-18 12:29:46 +0300

    revert, if statement and mapping added

--- more/36_Payable1.sol
+++ more/36_Payable1.sol
@@ -3,8 +3,9 @@
 pragma solidity >=0.8.7;
 
 contract SendEther {
-    function sendEth() external payable {
-
+    function investEthToThisContract() external payable{
+        //you dont need specify anything here, on remix panel, 
+        // you just need to insert value in value area and click on function button
     }
 
     function balanceOf() external view returns(uint) {
--- more/37_Payable2.sol
+++ more/37_Payable2.sol
@@ -2,11 +2,13 @@
 pragma solidity >=0.8.7;
 
 contract Payable2 {
-    function sentEth() external payable{
-
+    function investEthToThisContract() external payable{
+        if(msg.value < 1 ether) {
+            revert();
+        } 
     }
 
-    function getBalance() external returns(uint) {
+    function getBalance() external view returns(uint) {
         return address(this).balance;
     }
 }
