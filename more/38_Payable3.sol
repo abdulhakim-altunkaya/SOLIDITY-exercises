afe24d29287d36c48977ea78b7f67ef459afd19a
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-18 23:53:29 +0300

    if statement added for payment function

--- more/38_Payable3.sol
+++ more/38_Payable3.sol
@@ -6,7 +6,11 @@ contract SendEth {
     function sendViaCall(address payable _to) public payable {
         // Call returns a boolean value indicating success or failure.
         // This is the current recommended method to use.
-        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
-        require(sent, "Failed to send Ether");
+        if (msg.value > 1 ether) {
+            (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+            require(sent, "Failed to send Ether");
+        } else {
+            revert("You need to send at least 1 ether");
+        }
     }
 }
