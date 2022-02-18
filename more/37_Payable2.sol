20b989819058b52af50d558768a4d6262a1c63ab
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-18 13:14:29 +0300

    send via call method is tested to send ether from smart contract to any address

--- more/37_Payable2.sol
+++ more/37_Payable2.sol
@@ -2,13 +2,20 @@
 pragma solidity >=0.8.7;
 
 contract Payable2 {
+    mapping(address => uint) accounts;
+
     function investEthToThisContract() external payable{
         if(msg.value < 1 ether) {
             revert();
         } 
+        accounts[msg.sender] = msg.value;
     }
 
     function getBalance() external view returns(uint) {
         return address(this).balance;
+    }
+
+    function checkTransaction() external view returns(uint) {
+        return accounts[msg.sender];
     }
 }
---
+++ more/38_Payable3.sol
@@ -0,0 +1,12 @@
+//SPDX-Licence-Identifier: MIT
+
+pragma solidity >=0.8.7;
+
+contract SendEth {
+    function sendViaCall(address payable _to) public payable {
+        // Call returns a boolean value indicating success or failure.
+        // This is the current recommended method to use.
+        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+}
