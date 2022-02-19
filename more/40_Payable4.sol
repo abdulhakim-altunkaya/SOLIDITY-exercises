fc930d43ceec86c3b1b982e6dd9be10071566c5d
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-19 23:05:38 +0300

    now contract base is finished and it is a piece of art. The contract first creates a mapping, then it adds to mapping with order number. Then just by entering order number, I can send the amount to the relevant account represented by order number

--- more/40_Payable4.sol
+++ more/40_Payable4.sol
@@ -3,12 +3,16 @@
 pragma solidity >=0.8.7;
 
 contract Payable4 {
-    mapping(uint => address) public accounts;
+    mapping(uint => address payable) public accounts;
 
-    function addToMapping(uint _myOrder, address _myAddress) external {
+    function addToMapping(uint _myOrder, address payable _myAddress) external {
         accounts[_myOrder] = _myAddress;
     }
 
+    function makeTransaction(uint _accountOrder) external payable {
+        address payable _to = accounts[_accountOrder];
+        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+        require(sent, "failed to send Ether");
+    }
 
 }
-
