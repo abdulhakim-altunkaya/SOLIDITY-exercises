9d3b414686f9316339e07bc011519521247e85b2
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-19 22:57:19 +0300

    mapping and function to add addresses to mapping created, together with a dynamic order

--- more/40_Payable4.sol
+++ more/40_Payable4.sol
@@ -1,11 +1,14 @@
-
-
 // SPDX-Licence-Identifier: MIT
 
 pragma solidity >=0.8.7;
 
 contract Payable4 {
     mapping(uint => address) public accounts;
+
+    function addToMapping(uint _myOrder, address _myAddress) external {
+        accounts[_myOrder] = _myAddress;
+    }
+
 
 }
 
