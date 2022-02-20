809a0160b5717a156b9d3eec0820b723ae036f8c
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-20 22:32:55 +0300

    a more complex contracts file created

--- more/42_Multicon3.sol
+++ more/42_Multicon3.sol
@@ -1,40 +1,50 @@
-// SPDX-Licence-Identifier: MIT
 pragma solidity >=0.8.7;
 
-contract LedgerBalance {
-   mapping(address => uint) public balances;
+contract C {
 
-   function updateBalance(uint newBalance) public {
-      balances[msg.sender] = newBalance;
+   uint private data;
+   uint public info;
+
+   //constructor
+   constructor() public {
+      info = 10;
+   }
+
+   function increment(uint a) private pure returns(uint) { 
+       return a + 1; 
+    }
+   
+   function updateData(uint a) public { 
+       data = a; 
+    }
+
+   function getData() public view returns(uint) { 
+       return data; 
+    }
+
+   function compute(uint a, uint b) internal pure returns (uint) { 
+       return a + b; 
+    }
+}
+//External Contract
+contract D {
+   function readData() public returns(uint) {
+      C c = new C();
+      c.updateData(7);         
+      return c.getData();
    }
 }
-contract Updater {
-   function updateBalance() public returns (uint) {
-      LedgerBalance ledgerBalance = new LedgerBalance();
-      ledgerBalance.updateBalance(10);
-      return ledgerBalance.balances(address(this));
+//Derived Contract
+contract E is C {
+   uint private result;
+   C private c;
+   
+   constructor() public {
+      c = new C();
+   }  
+   function getComputedResult() public {      
+      result = compute(3, 5); 
    }
-}
-
-
-contract A {
-
-    address myAddress;
-
-    function setAddress(address _newContract) external {
-        myAddress = _newContract;
-    }
-
-    function getValue(uint _testNumber2) external view returns(uint) {
-        B b = B(myAddress);
-        return b.getValue(_testNumber2);
-    }
-  
-
-}
-
-contract B {
-    function getValue(uint _testNumber) external pure returns(uint){
-        return _testNumber;
-    }
+   function getResult() public view returns(uint) { return result; }
+   function getData() public view returns(uint) { return c.info(); }
 }
