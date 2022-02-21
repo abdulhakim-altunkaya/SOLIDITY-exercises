fe7301e39bdd764126b331e8fd721632e99ffba0
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-21 22:56:37 +0300

    exercises has been studied with burger size example

---
+++ more/44_enum.sol
@@ -0,0 +1,19 @@
+//SPDX-Licence-Identifier: MIT
+
+pragma solidity >=0.8.7;
+
+contract EnumExample {
+    enum BurgerSizes {CHILDREN, SIZE1, SIZE2, SIZE3};
+    BurgerSizes choice;
+    BurgerSizes constant defaultchoice = BurgerSizes.SIZE2;
+
+    function setLarge() external {
+        choice = BurgerSizes.SIZE3;
+    }
+    function setMedium() external {
+        choice = BurgerSizes.SIZE2;
+    }
+    function getSize() external view returns(BurgerSizes) {
+        return choice;
+    }
+}
