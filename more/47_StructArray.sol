9fef1e2f1868127db2ac6f076a0a32f98b2d9d0d
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-23 23:52:07 +0300

    first part of array integrated

--- more/47_StructArray.sol
+++ more/47_StructArray.sol
@@ -14,13 +14,24 @@ contract StructArray {
         flower = Flowers("rose", "white");
     }
 
+    Flowers[] myArray;
+    function addArray() external {
+        myArray.push(flower);
+    }
+
+    function getArray() external view returns(Flowers[] memory) {
+        return myArray;
+    }
+
     //you cannot return a record of a struct. You can only
     // return an individual data type in struct. In this below
     // it is flower name which is a string.
-    function getStruct() external view returns(string memory) {
+    function getName() external view returns(string memory) {
         return flower.name;
     }
 
-    function getStructValue() external view returns(string memory) {
+    function getColor() external view returns(string memory) {
         return flower.color;
+    }
+
 }
