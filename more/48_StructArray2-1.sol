3fe6b8cd1f9050929e28c3557d2b72e3e5c24160
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-02-26 23:02:38 +0300

    a new array exercises created and tested. It has function parameters to update array.

--- more/48_StructArray2-1.sol
+++ more/48_StructArray2-1.sol
@@ -6,16 +6,4 @@ contract TodosList {
         string task;
         bool compeleted;
     }
-
-    Todos[] public todosArray;
-
-    function addTasks(string memory task, bool completion) external {
-        todosArray.push(Todos(task, completion));
-    }
-
-    //Here we cannot return all record, I mean only task info is returned.
-    // But when I search the same thing on array, I can return both task and completed info
-    function getValues(uint indexNumber) external view returns(string memory) {
-        return todosArray[indexNumber].task;
-    }
 }
