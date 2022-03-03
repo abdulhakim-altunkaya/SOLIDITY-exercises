f1f8b58d880d4d275356dae31d27e932e5c6e4d3
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-03-03 17:16:09 +0300

    hexademical contract created

---
+++ more2/54_0x.sol
@@ -0,0 +1,9 @@
+//SPDX-Licence-Identifier: MIT
+
+pragma solidity >=0.8.7;
+
+contract HexTest{
+    function getAddress() external view returns(address memory) {
+        return address(0);
+    }
+}
