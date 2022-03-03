67b59e2ebd620a50ce24eab56417bf0e461b5b72
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-03-03 17:27:18 +0300

    0x hex prefix further studied

--- more2/54_0x.sol
+++ more2/54_0x.sol
@@ -3,7 +3,10 @@
 pragma solidity >=0.8.7;
 
 contract HexTest{
-    function getAddress() external view returns(address memory) {
+    function getAddress() external pure returns(address) {
         return address(0);
     }
+
+    bytes32 public myWord =bytes32("Hello");
+    bytes32 public myWord2 = bytes32(0);
 }
