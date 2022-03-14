a6e6ef8ff1f9026edac24766af5c3233b05b5a3c
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-03-14 15:16:44 +0300

    test function created, remove methods tested

--- more3/75_ArrayOperations.sol
+++ more3/75_ArrayOperations.sol
@@ -33,32 +33,6 @@ contract ArrayOperations {
         delete myArray[m];
     }
 
-    // REMOVE ELEMENT WAY 1
-    // To pop an element from any place inside the array:
-    // require statements makes sure our for loop will not collapse
-    // if somebody tries to delete an index higher than the array length
-    // if our array has only 1 element (in this case, "0<0" is fine actually, it wont give error)
-    function removeArray1(uint x) external {
-        require(myArray.length > 0, "array has no value");
-        require(x < myArray.length, "you can delete only existing elements");
-        uint myLength = myArray.length - 1;
-        for(uint myIndex = x; myIndex<myLength; myIndex++){
-            myArray[myIndex] = myArray[myIndex+1];
-        }
-        myArray.pop();
-    }
-
-    // REMOVE ELEMENT WAY 2
-    // This way will remove the element but values INDEXES WILL BE MIXED. Last element will 
-    // change place with target element. Target element will then be removed.
-    function removeArray2(uint x) external {
-        uint lastElIndex = myArray.length - 1;
-        uint lastElValue = myArray[lastElIndex];
-        myArray[lastElIndex] = myArray[x];
-        myArray[x] = lastElValue;
-        myArray.pop();
-    }
-
     //This is a regular return for dynamic array
     // Returning a big array can drain all your gas. Keep this in mind in production.
     function getArray() external view returns(uint[] memory) {
