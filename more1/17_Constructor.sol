939f56e0d24e53a7d978d4f7a287976291e66e28
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-03-20 01:13:26 +0300

    keccak bytes and abi.encodePacked contract has been rebuilt

--- more1/17_Constructor.sol
+++ more1/17_Constructor.sol
@@ -4,7 +4,7 @@ contract Constructor {
     string public myWord;
     string public myWord2;
 
-    constructor() public {
+    constructor() {
         myWord = "Word1: Constructor works";
         myWord2 = "Word2: Solidity is nice";
     }
