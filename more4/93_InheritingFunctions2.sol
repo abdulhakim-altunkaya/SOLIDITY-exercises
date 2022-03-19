0980ffc5172c515ff344da3189c718895a78159e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-03-19 20:51:25 +0300

    visibility contract created to test function-variable visibility tags and which ones a child will have an access

--- more4/93_InheritingFunctions2.sol
+++ more4/93_InheritingFunctions2.sol
@@ -13,10 +13,16 @@ contract A {
     function baz() public virtual {
         myword = "something A";
     }
-    //WAY-2: events
+    //WAY-2.1: events
     event Log(string mytext);
     function foo() public virtual {
         emit Log("hello from A");
+    }
+
+    //WAY-2.2: events
+    event Log2(string mytext);
+    function foo2() public virtual {
+        emit Log2("hello from A");
     }
 
     //WAY-3: return functions
@@ -26,18 +32,31 @@ contract A {
 }
 
 contract B is A{
-    
+    //WAY-3
     function bar() public pure override returns(string memory) {
         A.bar();
         return "good evening from B";
     }
+
+    //WAY 1
     function baz() public override {
         myword = "something B";
         A.baz();
     }
 
-    function foo() public virtual override {
+    //WAY 2.1
+    function foo() public override {
         emit Log("hello from B");
         A.foo();
     }
+
+    // WAY 2.2
+    function foo2() public override {
+        emit Log2("hello from B");
+        super.foo();
+    }
+
+    /* super.foo() and A.foo() are the same. super is more handy because it allows us to access 
+    all contracts that we inherit(imageine we are inheriting from 3 contracts instead of just A.
+    So, instead of writing the name of the contract each single time, I can just say "super" */
 }
