def3c7394c59e72192b8b695c16553722513f996
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-03-21 15:26:12 +0300

    array in array contract created and tested

--- more3/55_payable5.sol
+++ more3/55_payable5.sol
@@ -3,22 +3,19 @@
 pragma solidity >=0.8.7;
 
 contract Payable5 {
-    //this address below can now send ether. What about
-    // receiving ether?
-    address payable public owner;
-    //in constructor, I can't say owner=msg.sender
-    // because owner is payable and msg.sender is not yet.
+    //PAYABLE keyword adds functionality of sending and receiving ether.
+
+    address payable public owner; // This address can SEND ether.
+
+    function deposit() external payable {} //This function can RECEIVE ether.
+
+    //in constructor, I can't say owner=msg.sender because owner is payable and msg.sender is not yet.
     // So I cast msg.sender to payable
     constructor(){
       owner = payable(msg.sender);  
     }
 
-    //Here we are sending ether to this function. What
-    // this means I am not sure I know.
-    function deposit() external payable {
-
-    }
-
+    //This function returns the balance of the contract that includes this function. 
     function getBalance() external view returns(uint) {
         return address(this).balance;
     }
