a1633ef0449dd5feb6796227ca28f7c72fe42b2f
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-04-03 00:14:38 +0300

    contract finished and tested, comments added

--- more5/117_Call.sol
+++ more5/117_Call.sol
@@ -10,10 +10,14 @@ contract TestCall {
 
     event LogSomething(string message2);
 
+    //I need to add this to make sure contract can get ether for any function call
     fallback() external payable {
         emit LogSomething("someone has sent ether");
     }
 
+    //This payable here only means that whenever we call this function, we can enter
+    // some values to msg.value area and send it to the function contract.
+    //Even we dont have fallback, the contract can receive ether by this function. 
     function foo(string memory message3, uint number2) external payable returns (bool, uint) {
         message1 = message3;
         number1 = number2;
@@ -24,23 +28,10 @@ contract TestCall {
         return address(this).balance;
     }
 
+    //call method requires function to be payable
     function sendEther(address payable _to) external payable {
-        (bool success, ) = _to.call{value: 123}("");
+        (bool success, ) = _to.call{value: msg.value}("");
         require(success, "sending ether failed");
-    }
-
-}
-
-contract Call {
-    bytes a;
-    function call1(address payable otherContract, string memory message4, uint number3) external {
-        TestCall(otherContract).foo(message4, number3);
-    }
-
-    function call2(address payable otherContract) external {
-        (bool success, bytes memory data) = otherContract.call{value: 3333, gas: 5000}(abi.encodeWithSignature("foo(string,uint256)","helloooo", 11111));
-        require(success, "failed to call2");
-        a = data;
     }
 
 }
