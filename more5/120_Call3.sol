88e79d76a3a703be9c03b0ced5fa1f22ed8258a9
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-04-05 20:48:10 +0300

    contract created to practice calling the function of another contract with multiple parameters. At the end of the function, state variables change.

---
+++ more5/120_Call3.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.7;
+
+contract TestCall {
+    string public message;
+    uint public x;
+
+    event Log(string message);
+
+    fallback() external payable{
+        emit Log("fallback triggered");
+    }
+
+    function foo(string memory _message, uint _x) external payable returns(bool, uint){
+        x = _x;
+        message = _message;
+        return (true, 999);
+    }
+}
+
+/*Here this is the meaning of the exercise actually. I am calling payable foo
+function from another contract. Fallback and event has no meaning for this exercise.*/
+contract Call {
+    function call1(address payable otherContract, string memory a, uint b) external {
+        TestCall(otherContract).foo(a, b);
+    }
+}
