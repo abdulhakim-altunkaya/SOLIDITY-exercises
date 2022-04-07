89257f6704b8cfd2505ada7b3454ce01b81d45eb
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-04-07 21:41:31 +0300

    delegatecall two options contracts uploaded

--- more6/126_G.sol
+++ more6/126_G.sol
@@ -2,46 +2,20 @@
 
 pragma solidity >=0.8.7;
 
-contract A{
+//DELEGATECALL - ABI.ENCODEWITHSIGNATURE
 
-    uint public num;
-    address public sender;
-    uint public value;
+contract G {
 
-    function setVars(uint _num) external payable{
-        num = _num;
-        sender = msg.sender;
-        value = msg.value;
-    }
-
-}
-
-contract B{
-    /*Make sure the order of variables is totally the same as above*/
-    uint public num;
-    address public sender;
-    uint public value;
-    /*
-    FUNCTION 1: If I call in below way, the variables in contract A will change.
-    function setVars(address _test, uint _num) external payable{
-        (bool success, ) = _test.call(abi.encodeWithSignature("setVars(uint256)", _num));
+    string public myWord = "Mardin";
+    
+    function foo(address otherContract, string memory x) external {
+        (bool success, ) = otherContract.delegatecall(abi.encodeWithSignature("changeWord(string)", x));
         require(success, "failed to call");
     }
-    */
-    //FUNCTION 2.1: If I call in below way, the variables in Contract B will change.
-    //execution cost: 72k wei.
-    function setVars(address _test, uint _num) external payable{
-        (bool success, ) = _test.delegatecall(abi.encodeWithSignature("setVars(uint256)", _num));
-        require(success, "failed to call");
-    }
-    //FUNCTION 2.2: execution cost is cheap comparatively, 35k wei
-    function foo(address _test, uint _myNumber) external payable {
-        (bool success, ) = _test.delegatecall(abi.encodeWithSelector(A.setVars.selector, _myNumber));
-        require(success, "faile to call");
-    }
 
-    /*Now I understand the importance of delegatecall. By seperating function implementation from the contract, 
-    you can later update the contract B without redeploying it. You will redeploy contract A instead. 
-    */
+    //The state of E does not change.
+    //The state of A changes.
+    //Import statement not used.
+
 }
 
