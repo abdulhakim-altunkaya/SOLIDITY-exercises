07292fb68927404aa77a4406d844480d7059c92e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-04-07 17:40:07 +0300

    delegatecall contract further detailed, I added some comparative functions to see its difference and gas cost efficiency.

--- more6/126_DelegateCall.sol
+++ more6/126_DelegateCall.sol
@@ -21,10 +21,24 @@ contract B{
     uint public num;
     address public sender;
     uint public value;
-
+    /*
+    FUNCTION 1: If I call in below way, the variables in contract A will change.
     function setVars(address _test, uint _num) external payable{
-        (bool success, ) = _test.call(abi.encodeWithSignature("setVars(uint)", _num));
+        (bool success, ) = _test.call(abi.encodeWithSignature("setVars(uint256)", _num));
         require(success, "failed to call");
     }
+    */
+    //FUNCTION 2.1: If I call in below way, the variables in Contract B will change.
+    //execution cost: 72k wei.
+    function setVars(address _test, uint _num) external payable{
+        (bool success, ) = _test.delegatecall(abi.encodeWithSignature("setVars(uint256)", _num));
+        require(success, "failed to call");
+    }
+    //FUNCTION 2.2: execution cost is cheap comparatively, 35k wei
+    function foo(address _test, uint _myNumber) external payable {
+        (bool success, ) = _test.delegatecall(abi.encodeWithSelector(A.setVars.selector, _myNumber));
+        require(success, "faile to call");
+    }
+
 }
 
