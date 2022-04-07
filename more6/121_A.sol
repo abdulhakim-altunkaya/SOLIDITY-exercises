136af0cd402ccd866a5925681a4608348f5b5cc3
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-04-07 21:39:52 +0300

    extensive contracts project finished. First files were uploaded, folder restructured

--- more6/121_A.sol
+++ more6/121_A.sol
@@ -9,3 +9,33 @@ contract A {
         myWord = a;
     }
 }
+
+/*
+
+                        1) Import-inheritance
+                        2) Interface
+                        3) regular Function Call
+                        4) call method - abi.encodeWithSignature
+                        5) call method - abi.encodeWithSelector
+                        6) delegatecall method - abi.encodeWithSignature
+                        7) delegatecall method - abi.encodeWithSelector
+ 
+
+---STATE OF A CHANGES----
+2) Interface
+3) regular Function Call
+4) call method - abi.encodeWithSignature
+5) call method - abi.encodeWithSelector
+
+----STATE OF B CHANGES----
+1) Import-inheritance
+6) delegatecall method - abi.encodeWithSignature
+7) delegatecall method - abi.encodeWithSelector
+
+----IMPORT STATEMENT IS USED----
+1) Import-inheritance
+3) regular Function Call
+5) call method - abi.encodeWithSelector
+7) delegatecall method - abi.encodeWithSelector
+
+ */
