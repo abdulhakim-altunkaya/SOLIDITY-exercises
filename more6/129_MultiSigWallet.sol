6c702812d45c76ee00d038d9fa1c8ccab3456c59
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-04-09 14:00:26 +0300

    a struct created to format transaction records. Array created to store these records. approval number defined. Owner list created with owner array and isOwner variable. Last mapping is to check the approval status of each owner for a certain transaction that we can look up by iits index number.

--- more6/129_MultiSigWallet.sol
+++ more6/129_MultiSigWallet.sol
@@ -8,12 +8,39 @@ contract MultiSigWallet {
     //Approve will fire when the transaction is approved.
     //Revoke will fire when owners change their mind and revoke approved transaction. (But I am not
     // sure here. revoke may probably mean rejecting transaction also).
-    //Execute will fire when there is sufficient amount approvals, then the contract can be executed.
+    //Execute will fire when there is sufficient amount approvals, then the contract(Does
+    // "contract" mean "transaction"?) can be executed.
     event Deposit(address indexed sender, uint amount);
     event Submit(uint indexed txId);
     event Approve(address indexed owner, uint indexed txId);
     event Revoke(address indexed owner, uint indexed txId);
     event Execute(uint indexed txId);
 
-    
+    //This struct will create transaction records. Then we will store these records in an array.
+    struct Transaction {
+        address to; // recipient of our transaction.
+        uint value;
+        bytes data; //data sent with the transaction.
+        bool executed; //if transaction is successfull or not.
+    }
+
+    //Defining the array of owners. Only the owners will be able to call most of the 
+    // functions above.
+    address[] public owners;
+
+    //If an address is owner of multisigwallet it will return true.
+    mapping(address => bool) public isOwner;
+
+    //number of approvals required to approve transaction.
+    //For our exercise, we will assume 3 owners and 2 approvals will be required.
+    uint public required;
+
+    //Here will record transactions (in Transaction struct format).
+    Transaction[] public transactions;
+
+    //Here we will need to define a mapping to save approvals for each transaction.
+    //uint is the index of the transaction. 
+    mapping(uint => mapping(address => bool)) public approved;
+
+
 }
