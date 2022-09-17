640e51827e1b5b1f9870b3b8d84e31383c468733
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-09-17 16:55:23 +0300

    mapping struct exercise is finalized

--- more6/148_mapping_struct.sol
+++ more6/148_mapping_struct.sol
@@ -2,29 +2,6 @@
 
 pragma solidity >=0.8.7;
 
-contract Voting {
-
-    string[] internal proposals;
-
-    function addProposal(string memory _proposal) external {
-        proposals.push(_proposal);
-    }
-
-    struct ResultStruct {
-        string proposalName;
-        uint yesVotes;
-        uint noVotes;
-    }
-    ResultStruct record;
-    mapping(uint => ResultStruct) public resultsMapping;
-    function createRecord(uint index, uint yesVotes, uint noVotes) external {
-        record = ResultStruct(proposals[index], yesVotes, noVotes);
-    }
-    function addResults(uint index) external {
-        resultsMapping[index] = record;
-    }
-
-}
 
 contract Cities {
 
