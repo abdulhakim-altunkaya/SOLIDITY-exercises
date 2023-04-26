9426362d8e539bd96efb5a21efa73f41ee0d2c2e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-04-27 01:19:34 +0300

    deployment and test passed

--- more7/161_Compound.sol
+++ more7/161_Compound.sol
@@ -7,7 +7,7 @@ contract Compound {
 
     function compound (uint principal, uint rate, uint periods) public pure returns (uint) {
         for ( uint i=0; i<periods; i++ ) {
-            uint yield = principal * rate/100 ;
+            uint yield = principal*rate/100 ;
             principal += yield;
         }
         return principal;
